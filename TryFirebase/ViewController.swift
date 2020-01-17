7a53fe285fea0ce4ed4f1a5cca1d87523f15e03c
Andrew Tantomo <atantomo@socialgood-foundation.com>
2020-01-17 10:21:28 +0900

    Initial Commit

---
+++ TryFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryFirebase
+//
+//  Created by Andrew Tantomo on 2020/01/17.
+//  Copyright © 2020 Social Good Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
