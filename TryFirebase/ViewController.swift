4ea783bd079c8c8487ed8d00ea0b79d86de596af
Andrew Tantomo <Andrew@Andrews-MacBook-Pro.local>
2020-01-19 22:11:04 +0900

    Add 2

--- TryFirebase/ViewController.swift
+++ TryFirebase/ViewController.swift
@@ -14,39 +14,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Analytics.setScreenName("MyScreenName", screenClass: "ViewController")
+        Analytics.setUserProperty("23", forName: "sg_age")
+        Analytics.setUserProperty("M", forName: "sg_gender")
     }
 
     @IBAction func firstButtonTapped(_ sender: Any) {
         Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
-        AnalyticsParameterItemID: "id-pink",
-        AnalyticsParameterItemName: "pink",
-        AnalyticsParameterContentType: "cont"
+        AnalyticsParameterItemID: "id-eBay",
+        AnalyticsParameterItemName: "eBay",
+        AnalyticsParameterContentType: "merchant",
+        "love": "love_eBay",
+        "heart": "heart_eBay"
         ])
     }
 
     @IBAction func secondButtonTapped(_ sender: Any) {
         Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
-        AnalyticsParameterItemID: "id-yellow",
-        AnalyticsParameterItemName: "yellow",
-        AnalyticsParameterContentType: "cont"
+        AnalyticsParameterItemID: "id-Amazon",
+        AnalyticsParameterItemName: "Amazon",
+        AnalyticsParameterContentType: "merchant",
+        "love": "love_Amazon",
+        "heart": "heart_Amazon"
         ])
     }
 
     @IBAction func thirdButtonTapped(_ sender: Any) {
         Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
-        AnalyticsParameterItemID: "id-green",
-        AnalyticsParameterItemName: "green",
-        AnalyticsParameterContentType: "cont"
+        AnalyticsParameterItemID: "id-Booking.com",
+        AnalyticsParameterItemName: "Booking.com",
+        AnalyticsParameterContentType: "merchant",
+        "love": "love_Booking.com",
+        "heart": "heart_Booking.com"
         ])
     }
 
     @IBAction func fourthButtonTapped(_ sender: Any) {
         Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
-        AnalyticsParameterItemID: "id-blue",
-        AnalyticsParameterItemName: "blue",
-        AnalyticsParameterContentType: "cont"
+        AnalyticsParameterItemID: "id-Rakuten",
+        AnalyticsParameterItemName: "Rakuten",
+        AnalyticsParameterContentType: "merchant",
+        "love": "love_Rakuten",
+        "heart": "heart_Rakuten"
         ])
     }
 
 }
 
+class SecondViewController: UIViewController {
+}
